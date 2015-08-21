5485fc4798c566f78b94000cf590bf880d0484b0
Joe Shaw <joe@joeshaw.org>
2015-08-21 12:02:39 -0400

    oops, forgot to filter by the Dash's MAC address

--- gyro.go
+++ gyro.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/mdlayher/arp"
 )
 
 const gyroURL = "http://saltmines.us/gyroup.php"
 const name = "Some button pusher"
+
+const dashMac = "74:c2:46:81:f2:ac"
 
 func main() {
 	if len(os.Args) < 2 {
@@ -21,6 +24,11 @@ func main() {
 	iface, err := net.InterfaceByName(os.Args[1])
 	if err != nil {
 		log.Fatal("InterfaceByName: ", err)
+	}
+
+	mac, err := net.ParseMAC(dashMac)
+	if err != nil {
+		log.Fatal("ParseMAC: ", err)
 	}
 
 	c, err := arp.NewClient(iface)
@@ -34,7 +42,11 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if p.Operation == arp.OperationRequest && p.SenderIP.Equal(net.IPv4zero) {
+		if p.Operation == arp.OperationRequest &&
+			p.SenderIP.Equal(net.IPv4zero) &&
+			p.SenderHardwareAddr.String() == mac.String() {
+
+			log.Printf("Gyro button pressed at %s!", time.Now())
 			resp, err := http.PostForm(gyroURL, url.Values{"user_name": {name}})
 			if err != nil {
 				log.Printf("Error updating count: %s", err)
